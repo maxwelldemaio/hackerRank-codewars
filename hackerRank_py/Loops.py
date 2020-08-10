d4c8de89ea2f69fbcb35cb2727b9e51ed937ba16
Maxwell DeMaio <maxdman321@gmail.com>
2020-08-10 00:29:59 -0400

    Updated readme and started adding previous codingbat files

--- hackerRank_py/Loops.py
+++
@@ -1,5 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-
-    for x in range(0, n):
-        print(x**2)
--- hackerRank_py/Print Function.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-
-    def between(x):
-        for i in range(1, x+1):
-            print(i, end="")
-
-between(n)
