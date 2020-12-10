cf26671f4141294fead5687e787a205cc1c8798c
Maxwell DeMaio <maxdman321@gmail.com>
2020-12-09 22:27:04 -0500

    Started reverse int

---
+++ leetcode_py/ReverseInteger.py
@@ -0,0 +1,34 @@
+"""
+Given a 32-bit signed integer, reverse digits of an integer.
+
+Note:
+Assume we are dealing with an environment that could 
+only store integers within the 32-bit signed integer range: [−231,  231 − 1]. 
+For the purpose of this problem, assume that your function returns 0 when 
+the reversed integer overflows.
+
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+"""
+
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        return
+        
+# Create solution instance
+l = Solution()
+
+a = 123
+b = -123
+
+# Tests (Lists are mutuable so technically changes the original list)
+l.reverse(a)
+l.reverse(b)
